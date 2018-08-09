c8c927dcfaab549244d8d1819123400c2c26c17d
adebeks92 <42240064+adebeks92@users.noreply.github.com>
2018-08-09 12:48:03 +0100

    My first commit!

---
+++ script.js
@@ -0,0 +1,33 @@
+//
+const myName = "Tobi Ruth"
+const myLastName = "Adebekun"
+const myFullName = myName + " " + myLastName
+
+const number1 = 4
+const number2 = 2 
+const sumNumbers = number1 + number2
+
+
+function flipCoin(threshold) {
+
+	const myRandomNumber = Math.random() 
+	if (myRandomNumber > threshold) {
+		alert("You won, " + myName)
+		alert("You really won!")
+	} else {
+		alert("You lost, " + myName)
+		alert("Loser")
+	}
+}
+
+function resizeButton() {
+	const newRadius = Math.random() * 20;
+	$('.button').css('border-radius',newRadius + 'px');
+	
+}
+
+function slideImage(){
+	$('.Bitmoji').slideUp();
+	$('.Bitmoji').slideDown();
+	
+	} 
